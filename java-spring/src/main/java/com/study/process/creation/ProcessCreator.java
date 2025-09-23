4ec19b2fb8b2e585bfb772702c564a3f92ee0bb7
jun <pickjog@gmail.com>
2025-09-24 05:46:07 +0900

    feat : create ProcessCreator

---
+++ java-spring/src/main/java/com/study/process/creation/ProcessCreator.java
@@ -0,0 +1,4 @@
+package com.study.process.creation;
+
+public class ProcessCreator {
+}
---
+++ java-spring/src/test/java/com/study/process/creation/ProcessCreatorTest.java
@@ -0,0 +1,7 @@
+package com.study.process.creation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProcessCreatorTest {
+
+}
