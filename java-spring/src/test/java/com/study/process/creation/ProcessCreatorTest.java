02b186182eae8c0059723525073d50858540ee04
jun <pickjog@gmail.com>
2025-09-25 05:05:03 +0900

    feat : setup test ProcessCreator

--- java-spring/src/test/java/com/study/process/creation/ProcessCreatorTest.java
+++ java-spring/src/test/java/com/study/process/creation/ProcessCreatorTest.java
@@ -1,7 +1,15 @@
 package com.study.process.creation;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProcessCreatorTest {
+    private ProcessCreator creator;
+
+    @BeforeEach
+    void setUp() {
+        creator = new ProcessCreator();
+    }
 
 }
